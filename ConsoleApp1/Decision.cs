9bf368db749df66233336fdbcca2d9f15a294cf4
Brian kimanzi <migelbrian3@gmail.com>
2025-02-06 22:19:08 +0300

    c# codes

---
+++ ConsoleApp1/Decision.cs
@@ -0,0 +1,21 @@
+namespace Decisions;
+
+ class Decisions
+{
+    static void main(string[] args)
+    {
+        Console.WriteLine("Brian's Give Away! ");
+        Console.Write("Choose a door: 1, 2, or 3: ");
+        string userValue = Console.Readline();
+
+        if (userValue == "1"):
+        {
+            string message = "you have won a new car!"
+            Console.WriteLine(message)
+        }
+    
+    Console.ReadLine();
+
+
+    }
+}
